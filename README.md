403a0df697ff3dc3b04444940eed840e6c63ab0c
xenxynon <flynryder427@gmail.com>
2022-05-21 07:22:29 +0000

    Add extras for qssi-user 12 SKQ1.210908.001 V13.0.1.0.SKGINXM release-keys
    Signed-off-by: xenxynon <flynryder427@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 12 SKQ1.210908.001 V13.0.1.0.SKGINXM release-keys
+- Manufacturer: xiaomi
+- Platform: 
+- Codename: qssi
+- Brand: qti
+- Flavor: qssi-user
+- Release Version: 12
+- Id: SKQ1.210908.001
+- Incremental: V13.0.1.0.SKGINXM
+- Tags: release-keys
+- CPU Abilist: 
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: qti/qssi/qssi:12/SKQ1.210908.001/V13.0.1.0.SKGINXM:user/release-keys
+- OTA version: 
+- Branch: qssi-user-12-SKQ1.210908.001-V13.0.1.0.SKGINXM-release-keys
+- Repo: qti_qssi_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cat vendor_bootimg/16_dtbdump_Ct!\.dtb.* 2>/dev/null >> vendor_bootimg/16_dtbdump_Ct!\.dtb
+rm -f vendor_bootimg/16_dtbdump_Ct!\.dtb.* 2>/dev/null
+cat vendor_bootimg/16_dtbdump_8,(%.dtb.* 2>/dev/null >> vendor_bootimg/16_dtbdump_8,(%.dtb
+rm -f vendor_bootimg/16_dtbdump_8,(%.dtb.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat vendor_boot.img.* 2>/dev/null >> vendor_boot.img
+rm -f vendor_boot.img.* 2>/dev/null
+cat system/system/priv-app/MIUIGalleryGlobalExplore/MIUIGalleryGlobalExplore.apk.* 2>/dev/null >> system/system/priv-app/MIUIGalleryGlobalExplore/MIUIGalleryGlobalExplore.apk
+rm -f system/system/priv-app/MIUIGalleryGlobalExplore/MIUIGalleryGlobalExplore.apk.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk.* 2>/dev/null >> system/system/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk
+rm -f system/system/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk.* 2>/dev/null
+cat system/system/app/MIUINotes/MIUINotes.apk.* 2>/dev/null >> system/system/app/MIUINotes/MIUINotes.apk
+rm -f system/system/app/MIUINotes/MIUINotes.apk.* 2>/dev/null
+cat system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk.* 2>/dev/null >> system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk
+rm -f system/system/app/MIUIGalleryPlayerService/MIUIGalleryPlayerService.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system_ext/apex/com.android.vndk.v30.apex
+rm -f system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
